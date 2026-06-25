dc0369d89a0663d3307df4f867018cd0cee06e95
Thiago-k22 <kovacsthiago45@gmail.com>
2026-06-25 15:49:34 -0300

    Update logica.py

--- src/logica.py
+++ src/logica.py
@@ -1,1 +1,18 @@
+from Clases import Guerrero, Mago, Arquero
 
+def crear_personaje(opcion, nombre):
+
+    if opcion == 1:
+        return Guerrero(nombre, 100, 15, 10)
+
+    elif opcion == 2:
+        return Mago(nombre, 80, 20, 50)
+
+    elif opcion == 3:
+        return Arquero(nombre, 90, 12, 20)
+
+    elif opcion == 4:
+        return Asesino(nombre, 60, 35, 25)
+
+    else:
+        return None
