ac4c9efc6f1fbce825eaa2d571336c06ee6d6430
Thiago-k22 <kovacsthiago45@gmail.com>
2026-06-25 22:13:36 -0300

    Update logica.py

--- src/logica.py
+++ src/logica.py
@@ -1,3 +1,4 @@
+import random
 from clases import Guerrero, Mago, Arquero, Asesino
 
 def crear_personaje(opcion, nombre):
@@ -16,3 +17,53 @@ def crear_personaje(opcion, nombre):
 
     else:
         return None
+
+def crear_enemigo():
+    enemigos = [
+        Guerrero("Orco", 100, 15, 0, 8),
+        Mago("Hechicero", 80, 20, 0, 40),
+        Arquero("Cazador", 90, 12, 0, 10),
+        Asesino("Bandido", 70, 18, 0, 0.25)
+    ]
+
+    return random.choice(enemigos)
+
+
+def combate(jugador, enemigo):
+
+    print("\n===== COMBATE =====")
+
+    while jugador.esta_vivo() and enemigo.esta_vivo():
+
+        print(f"\n{jugador.get_nombre()} ({jugador.get_salud()} HP)")
+        print(f"{enemigo.get_nombre()} ({enemigo.get_salud()} HP)")
+
+        print("\n1. Atacar")
+        print("2. Habilidad especial")
+        print("3. Usar poción")
+
+        opcion = input("Seleccione una acción: ")
+
+        if opcion == "1":
+            jugador.atacar(enemigo)
+
+        elif opcion == "2":
+            jugador.habilidad_especial(enemigo)
+
+        elif opcion == "3":
+            jugador.usar_pociones()
+
+        else:
+            print("Opción inválida.")
+            continue
+
+        if enemigo.esta_vivo():
+            print(f"\nTurno de {enemigo.get_nombre()}")
+            enemigo.atacar(jugador)
+
+    print("\n===== FIN DEL COMBATE =====")
+
+    if jugador.esta_vivo():
+        print(f"¡{jugador.get_nombre()} ganó la batalla!")
+    else:
+        print(f"{enemigo.get_nombre()} ganó la batalla.")
